bf8f907a47c4f6a79b7482469f1f607adfb2c901
Esturban <valtaye@gmail.com>
2021-05-06 20:30:38 -0400

    Update to CSS Selector
    Changing the selector to the most recent version of it
    
    Getty images may occassionally change the CSS class amid UI updates

--- src/4_imgs.R
+++ src/4_imgs.R
@@ -2,6 +2,6 @@
 'https://www.gettyimages.com/photos/taylor-swift?family=editorial&phrase=taylor%20swift&sort=mostpopular' %>%
   read_html %>%
   html_nodes(
-    'body > div.content_wrapper > section > div > main > section > div.search-content__gallery-pager-wrapper > div > div.search-content__gallery > div > div > div.search-content__gallery-assets > div > article > a > figure > img'
+    'body > div.content_wrapper > section > div > main > section > div.search-content__gallery-pager-wrapper > div > div > div > div.search-content__gallery-assets > div > article > a > figure > img'
   ) %>%
   html_attr('src') -> ts_imgs
